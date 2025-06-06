f7e2b46f0b949b0035aba520ed33e6e46777a781
vyfor <vyforium@gmail.com>
2025-06-06 06:42:31 +0500

    chore: fix warnings

--- src/util/log.rs
+++ src/util/log.rs
@@ -4,7 +4,7 @@ use directories::ProjectDirs;
 use lazy_static::lazy_static;
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{
-    self, layer::SubscriberExt, util::SubscriberInitExt, Layer,
+    self, Layer, layer::SubscriberExt, util::SubscriberInitExt,
 };
 
 lazy_static! {
@@ -24,14 +24,13 @@ fn project_directory() -> Option<ProjectDirs> {
 }
 
 pub fn get_data_dir() -> PathBuf {
-    let directory = if let Some(s) = DATA_FOLDER.clone() {
+    if let Some(s) = DATA_FOLDER.clone() {
         s
     } else if let Some(proj_dirs) = project_directory() {
         proj_dirs.data_local_dir().to_path_buf()
     } else {
         PathBuf::from(".").join(".data")
-    };
-    directory
+    }
 }
 
 pub fn initialize_logging() -> color_eyre::Result<()> {
@@ -39,12 +38,16 @@ pub fn initialize_logging() -> color_eyre::Result<()> {
     std::fs::create_dir_all(directory.clone())?;
     let log_path = directory.join(LOG_FILE.clone());
     let log_file = std::fs::File::create(log_path)?;
-    std::env::set_var(
-        "RUST_LOG",
-        std::env::var("RUST_LOG")
-            .or_else(|_| std::env::var(LOG_ENV.clone()))
-            .unwrap_or_else(|_| format!("{}=info", env!("CARGO_CRATE_NAME"))),
-    );
+    unsafe {
+        std::env::set_var(
+            "RUST_LOG",
+            std::env::var("RUST_LOG")
+                .or_else(|_| std::env::var(LOG_ENV.clone()))
+                .unwrap_or_else(|_| {
+                    format!("{}=info", env!("CARGO_CRATE_NAME"))
+                }),
+        );
+    }
     let file_subscriber = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
