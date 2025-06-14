2ab3625faac070300331d9ac91b82e4b3913ae5c
vyfor <vyforium@gmail.com>
2025-06-14 13:21:42 +0500

    feat: create a custom source for real-time effect chaining

---
+++ src/audio/fx.rs
@@ -0,0 +1,68 @@
+use rodio::Source;
+use std::time::Duration;
+
+pub trait Fx: Send + 'static {
+    fn process(&mut self, sample: f32) -> f32;
+}
+
+impl<F: FnMut(f32) -> f32 + Send + 'static> Fx for F {
+    fn process(&mut self, sample: f32) -> f32 {
+        (self)(sample)
+    }
+}
+
+pub struct FxSource<T: Source<Item = f32> + Send + 'static> {
+    inner: T,
+    effects: Vec<Box<dyn Fx>>,
+}
+
+impl<T: Source<Item = f32> + Send + 'static> FxSource<T> {
+    pub fn new(inner: T) -> Self {
+        Self {
+            inner,
+            effects: Vec::new(),
+        }
+    }
+
+    pub fn add_effect<E: Fx>(&mut self, effect: E) {
+        self.effects.push(Box::new(effect));
+    }
+
+    pub fn clear_effects(&mut self) {
+        self.effects.clear();
+    }
+}
+
+impl<T: Source<Item = f32> + Send + 'static> Source for FxSource<T> {
+    fn current_span_len(&self) -> Option<usize> {
+        self.inner.current_span_len()
+    }
+
+    fn channels(&self) -> u16 {
+        self.inner.channels()
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.inner.sample_rate()
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        self.inner.total_duration()
+    }
+
+    fn try_seek(&mut self, pos: Duration) -> Result<(), rodio::source::SeekError> {
+        self.inner.try_seek(pos)
+    }
+}
+
+impl<T: Source<Item = f32> + Send + 'static> Iterator for FxSource<T> {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<f32> {
+        let mut sample = self.inner.next()?;
+        for effect in &mut self.effects {
+            sample = effect.process(sample);
+        }
+        Some(sample)
+    }
+}
