efd815987723d0054d93305106d19782c7605c5d
vyfor <vyfor@proton.me>
2025-11-20 13:08:12 +0500

    style(colors): update `ACCENT` color value

--- src/util/colors.rs
+++ src/util/colors.rs
@@ -4,4 +4,4 @@ pub const PRIMARY: Color = Color::from_u32(0x00f7d44b);
 pub const SECONDARY: Color = Color::from_u32(0x009D8400);
 pub const NEUTRAL: Color = Color::from_u32(0x00404040);
 pub const BACKGROUND: Color = Color::from_u32(0x000d0d0d);
-pub const ACCENT: Color = Color::from_u32(0x00FFFFFF);
+pub const ACCENT: Color = Color::from_u32(0x00feca88);
